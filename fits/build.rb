796a810c6fe8fb3ca022b370ae30292d686b57e7
Edward Paget <ed.paget@gmail.com>
2013-05-10 13:06:56 -0500

    Fix to build on case-sensitive file-systems

--- fits/build.rb
+++ fits/build.rb
@@ -5,7 +5,7 @@ fits_app_dir = File.expand_path File.dirname(__FILE__)
 
 Dir.chdir fits_app_dir
 `../node_modules/hem/bin/hem build`
-`coffee -b -c -o ../public workers`
+`../node_modules/coffee-script/bin/coffee -b -c -o ../public workers`
 `../node_modules/uglify-js/bin/uglifyjs --overwrite ../public/fits_workers.js`
 `mv public/application.js ../public/fits.js`
 `rm public/application.css`
